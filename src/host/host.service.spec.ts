5e8949115d618bfafde933ba401d27b5fdbdcd47
kristoffer <avion@mailfence.com>
2023-05-08 21:39:13 +0200

    Update host controller and service tests

--- src/host/host.service.spec.ts
+++ src/host/host.service.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HostService } from './host.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HostEntity } from './host.entity/host.entity';
+import config from '../../ormconfig';
 
 describe('HostService', () => {
   let service: HostService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(config),
+        TypeOrmModule.forFeature([HostEntity]),
+      ],
       providers: [HostService],
     }).compile();
 
