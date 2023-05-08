8415bdc3570850e82c351436b8715417f17b1e99
kristoffer <avion@mailfence.com>
2023-05-08 19:12:20 +0200

    Initial commit

--- src/host/host.service.ts
+++ src/host/host.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { HostEntity } from './host.entity/host.entity';
-import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class HostService {
